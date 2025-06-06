f5fc5cc4820f2bae9a8b8a6fd9d44f3dfce537d2
Valkon <96625039+Nawija@users.noreply.github.com>
2025-06-06 21:40:44 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    experimental: {
-        mdxRs: false,
-    },
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
